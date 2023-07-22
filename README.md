2ee6c058f95183d16ae253cb4271f96e3e7a255d
Alex Portell <mail@example.com>
2023-07-22 01:27:17 -0400

    Added class to control lists. Renamed classes. Consolidated work into new class. Added header comments.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # pwrstat-virtman
-Suspend or resume (sleep or wake) a given QEMU/KVM virtual machine. Simple GTK3 GUI application.
+Set power state of a given QEMU/KVM virtual machine. Includes sleep, hibernate, and wake from sleep. Simple GTK3 GUI application.
 
 # Why?
 My daily driver application *virt-manager* does not (currently) have the option to wake a VM from sleep. That is unfortunate, as my VM cannot be awaken from normal and expected means (wake from USB keyboard or network activity).
--- pwrstat-virtman.py
+++ pwrstat-virtman.py
@@ -1,6 +1,19 @@
+#!/usr/bin/env python3
+
+###########################################################
+#
+# Filename:       pwrstat-virtman.py
+# Description:    Set power state of a given QEMU/KVM virtual machine. Includes sleep, hibernate, and wake from sleep.
+# Author(s):      Alex Portell <github.com/portellam>
+# Maintainer(s):  Alex Portell <github.com/portellam>
+#
+###########################################################
+
 from os import system
 
-class BashEnum:
+glade_file_name="pwrstat-virtman"
+
+class Bash_Enum:
   enum = []
 
   def __init__(enum):
@@ -33,7 +46,7 @@ class BashEnum:
       __init__()
       sys.exit(1)
 
-class BashVar:
+class Bash_Var:
   var = ""
 
   def __init__(var):
@@ -64,75 +77,6 @@ class BashVar:
       exception_message = "Exception: Failed to set variable."
       print(exception_message)
       __init__()
-      sys.exit(1)
-
-class DomainList:
-  command_prefix        = "sudo virsh"
-
-  option_list_all       = "list --all"
-  option_set_domain     = "--domain"
-  option_set_target     = "--target"
-  option_target_both    = "hybrid"
-  option_target_disk    = "disk"
-  option_target_memory  = "mem"
-
-  state_paused    = "paused"
-  state_started   = "running"
-  state_stopped   = "shut off"
-  state_stop      = "pmsuspended"
-
-  def __init__(self, domain_list, domain_dict):
-    self.domain_list = domain_list
-    self.domain_dict = domain_dict
-
-  def get_domain_status():
-    if self.domain not in self.domain_list:
-      syscall.exit(1)
-
-    i = self.domain_list.index(self.domain)
-    self.status = self.status_list(i)
-
-  def get_lists():
-    __get_domain_list()
-    __get_domain_dict()
-
-  def __get_domain_list():
-    list_ref = "_ARR_DOMAIN"
-    get_domain_list = "{} {} {}".format(command_prefix, option_list_all, option_set_domain)
-    set_ref = "declare -a {}=$( {} )".format(list_ref, get_domain_list)
-    subprocess.run(set_ref)
-    BashClass.get_enum_from_reference(list_ref)
-    self.domain_list = BashClass.enum
-
-    if self.domain_list is None:
-      sys.exit(1)
-
-  def __get_domain_dict():
-    domain_ref = "_DOMAIN"
-    element_ref = "_ELEMENT"
-    get_domain_list = "{} {} {}".format(command_prefix, option_list_all, option_set_domain)
-    get_messy_domain_and_state = "{} {} | grep '${}' | head -n 1 '".format(command_prefix, option_list_all, domain_ref)
-
-    for domain in self.domain_list:
-      set_domain = "declare -a {}=\"{}\"".format(domain_ref, domain)
-      status = ""
-
-      while True:
-        index=2
-        get_delimited_value = "| awk 'END {print $" + index + "}'"
-        get_element = "declare {}=$( \"{}{}\" )".format(element_ref, get_messy_domain_and_state, get_delimited_value)
-        subprocess.run(get_element)
-        BashVarController.set_var_from_reference(element_ref)
-
-        if BashVarController.var is None:
-          break
-
-        status.__add__("{} ".format(BashVarController.var))
-        index.__add__(1)
-
-      self.domain_dict.append([domain, status])
-
-    if self.domain_dict is None:
       sys.exit(1)
 
 class Domain:
@@ -214,3 +158,99 @@ class Domain:
     except:
       exception_message = "An exception occurred."
       print(exception_message)
+
+class Domain_List:
+  command_prefix        = "sudo virsh"
+
+  option_list_all       = "list --all"
+  option_set_domain     = "--domain"
+  option_set_target     = "--target"
+  option_target_both    = "hybrid"
+  option_target_disk    = "disk"
+  option_target_memory  = "mem"
+
+  state_paused    = "paused"
+  state_started   = "running"
+  state_stopped   = "shut off"
+  state_stop      = "pmsuspended"
+
+  def __init__(self, domain_list, domain_dict):
+    self.domain_list = domain_list
+    self.domain_dict = domain_dict
+
+  def get_lists():
+    __get_domain_list()
+    __get_domain_dict()
+
+  def __get_domain_list():
+    list_ref = "_ARR_DOMAIN"
+    get_domain_list = "{} {} {}".format(command_prefix, option_list_all, option_set_domain)
+    set_ref = "declare -a {}=$( {} )".format(list_ref, get_domain_list)
+    subprocess.run(set_ref)
+    BashClass.get_enum_from_reference(list_ref)
+    self.domain_list = BashClass.enum
+
+    if self.domain_list is None:
+      sys.exit(1)
+
+  def __get_domain_dict():
+    domain_ref = "_DOMAIN"
+    element_ref = "_ELEMENT"
+    get_domain_list = "{} {} {}".format(command_prefix, option_list_all, option_set_domain)
+    get_messy_domain_and_state = "{} {} | grep '${}' | head -n 1 '".format(command_prefix, option_list_all, domain_ref)
+
+    for domain in self.domain_list:
+      set_domain = "declare -a {}=\"{}\"".format(domain_ref, domain)
+      status = ""
+
+      while True:
+        index=2
+        get_delimited_value = "| awk 'END {print $" + index + "}'"
+        get_element = "declare {}=$( \"{}{}\" )".format(element_ref, get_messy_domain_and_state, get_delimited_value)
+        subprocess.run(get_element)
+        Bash_VarController.set_var_from_reference(element_ref)
+
+        if Bash_VarController.var is None:
+          break
+
+        status.__add__("{} ".format(Bash_VarController.var))
+        index.__add__(1)
+
+      self.domain_dict.append([domain, status])
+
+    if self.domain_dict is None:
+      sys.exit(1)
+
+class Glade_Python_Gtk(Gtk.Window):
+  column_list = [ "Name" "Status" ]
+
+  def __init__(self):
+    builder = Gtk.Builder()
+    builder.add_from_file(glade_file_name)
+    builder.connect_signals(self)
+
+    Domain_List.get_lists()
+
+    self.ListStore1 = builder.get_object('ListStore1')
+    self.TreeView1 = builder.get_object('TreeView1')
+
+    for i in range(column_list):
+      column = Gtk.TreeViewColumn(column_list[i], Gtk.CellRendererText(), text=i)
+      column.set_clickable(True)
+      column.set_resizable(True)
+      self.TreeView1.append_column(column)
+
+    for domain_and_status in Domain_List.domain_dict:
+      self.ListStore1.append(list(domain_and_status))
+
+    window = builder.get_object('ViewPort1')
+    window.connect('delete-event', Gtk.main_quit)
+    window.show_all()
+
+  def on_window1_destroy(self, object, data=None):
+    print ("Quit with exit.")
+    Gtk.main_quit()
+
+if __name__ == "__main__":
+  main = Glade_Python_Gtk()
+  Gtk.main()
