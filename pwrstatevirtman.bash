79b55500f18f3ec772fe77d74ddb2e156040c439
portellam <amportell.13@gmail.com>
2024-07-05 23:01:34 -0400

    Create pwrstatevirtman.bash

---
+++ pwrstatevirtman.bash
@@ -0,0 +1,9 @@
+#!/bin/bash/env bash
+
+#
+# Filename:       pwrstatevirtman.bash
+# Description:    Bash commandline tool for Power State Virtual Machine Manager.
+# Author(s):      Alex Portell <github.com/portellam>
+# Maintainer(s):  Alex Portell <github.com/portellam>
+#
+
