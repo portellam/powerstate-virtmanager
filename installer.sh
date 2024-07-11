a2fa23315d49f2659692fdca4a43aa1021554fb0
portellam <amportell.13@gmail.com>
2024-07-11 00:45:47 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,18 +1,68 @@
 #!/bin/bash
 
-local -r str_script_file="powerstate-virtmanager-script-dialog.sh"
-local -r str_source_file="script-dialog"
+#
+# Filename:       installer.sh
+# Version:        1.0.0
+# Description:    Install executable and source files.
+# Author(s):      Alex Portell <github.com/portellam>
+# Maintainer(s):  Alex Portell <github.com/portellam>
+#
 
-if [[ -e "${str_script_file}" ]]; then
-  echo "Error: Cannot locate script file."
-  return 1
-fi
+function install
+{
+  local -r script_file="powerstate-virtmanager.sh"
 
-if [[ -e "${str_source_file} ]]; then
-  echo "Error: Cannot locate source file."
-  return 1
-fi
+  if [[ -e "./${script_file}" ]]; then
+    echo "Error: Cannot locate script file."
+    return 1
+  fi
 
-local 
+  local -r source_file="powerstate-virtmanager.d/script-dialog"
 
-if ! cp 
+  if [[ -e "./${source_file}" ]]; then
+    echo "Error: Cannot locate source file."
+    return 1
+  fi
+
+  local -r destination_path="/usr/bin/local"
+
+  if ! cp "./${script_file}" "${destination_path}/${script_file}" \
+    &> /dev/null; then
+    echo "Error: Cannot copy script file."
+    return 1
+  fi
+
+  if ! cp "./${source_file}" "${destination_path}/${source_file}" \
+    &> /dev/null; then
+    echo "Error: Cannot copy source file."
+    return 1
+  fi
+
+  if ! chmod +x "./${script_file}" "${destination_path}/${script_file}" \
+    &> /dev/null; then
+    echo "Error: Cannot set script file as executable."
+    return 1
+  fi
+
+  if ! chmod +x "./${source_file}" "${destination_path}/${source_file}" \
+    &> /dev/null; then
+    echo "Error: Cannot set source file as executable."
+    return 1
+  fi
+
+  return 0
+}
+
+function main
+{
+  if ! install; then
+    echo "Install failed."
+    return 1
+  fi
+
+  echo "Install successful."
+  return 0
+}
+
+main
+exit "${?}"
