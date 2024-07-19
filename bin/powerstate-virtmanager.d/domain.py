320c1d19c638ef97aebaf81f23b3a361bbe226a5
portellam <amportell.13@gmail.com>
2024-07-19 03:12:23 -0400

    Create domain.py

---
+++ bin/powerstate-virtmanager.d/domain.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+#
+# Filename:       domain.py
+# Version:        1.0.0
+# Description:    Virtual Machine logic.
+# Author(s):      Alex Portell <github.com/portellam>
+# Maintainer(s):  Alex Portell <github.com/portellam>
+#
