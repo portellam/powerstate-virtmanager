4b7e4572eba04c8172e82c8c2d724a444d66437f
portellam <amportell.13@gmail.com>
2024-07-19 03:14:06 -0400

    Create bash_datatypes.py

---
+++ bin/powerstate-virtmanager.d/bash_datatypes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+#
+# Filename:       bash_datatypes.py
+# Version:        1.0.0
+# Description:    Datatype logic from Bash to Python.
+# Author(s):      Alex Portell <github.com/portellam>
+# Maintainer(s):  Alex Portell <github.com/portellam>
+#
