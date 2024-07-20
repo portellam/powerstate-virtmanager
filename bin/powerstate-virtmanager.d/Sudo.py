0c03b254f20c14c40651415423615c5416fe6453
portellam <amportell.13@gmail.com>
2024-07-20 00:24:32 -0400

    Rename and added imports.

---
+++ bin/powerstate-virtmanager.d/Sudo.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python
+
+#
+# Filename:       Sudo.py
+# Version:        1.0.0
+# Description:    Determine if the Python script was executed by a sudo user.
+# Author(s):      Alex Portell <github.com/portellam>
+# Maintainer(s):  Alex Portell <github.com/portellam>
+#
+
+import BashCommand
+
+class Sudo:
+  is_sudo = false
+  command = "sudo"
+
+  def __init__():
+    is_sudo = BashCommand.get_command_return_code(command) == 0
+    self.is_sudo = is_sudo
--- bin/powerstate-virtmanager.d/sudo.py
+++
@@ -1,6 +0,0 @@
-class sudo:
-  is_sudo = false
-  command = "sudo"
-
-  def __init__(is_sudo):
-    self.is_sudo = is_sudo
