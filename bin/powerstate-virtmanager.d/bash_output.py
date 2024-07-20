cf087967fcc556978c5f49c02bb2610ad1906e18
portellam <amportell.13@gmail.com>
2024-07-19 23:39:18 -0400

    Update bash_output.py

--- bin/powerstate-virtmanager.d/bash_output.py
+++ bin/powerstate-virtmanager.d/bash_output.py
@@ -11,31 +11,45 @@
 import subprocess
 import sys
 
-def get_command_output(command):
-  if command is None:
-    sys.exit(1)
-
-  try:
-      result = subprocess.check_output(
-        command,
-        shell = True,
-        stderr = subprocess.STDOUT,
-        text = True
-      )
-
+class bash_output:
+  def get_command_input():
+    try:
+      result = sys.stdin.readline()
       return result
 
-  except subprocess.CalledProcessError as exception:
-      print(exception)
+    except subprocess.CalledProcessError as exception:
+        print(exception)
+        sys.exit(1)
+
+  def get_command_output(command):
+    if command is None:
       sys.exit(1)
 
-def get_string_literal(string):
-  return  "\"{}\"" \
-          .format(string)
+    try:
+        result = subprocess.check_output(
+          command,
+          shell = True,
+          stderr = subprocess.STDOUT,
+          text = True
+        )
 
-text = sys.stdin.readline()
+        return result
 
-expression =  "echo {}" \
-              .format(text)
+    except subprocess.CalledProcessError as exception:
+        print(exception)
+        sys.exit(1)
 
-print(get_command_output(expression))
+  def get_string_literal(string):
+    return  "\"{}\"" \
+            .format(string)
+
+  # def get_variable(reference):
+  #   return  "\"{}\"" \
+  #           .format(string)
+
+  text = get_command_input()
+
+  expression =  "echo {}" \
+                .format(text)
+
+  print(get_command_output(expression))
