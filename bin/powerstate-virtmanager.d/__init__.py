17d76324215ffc49e95b68a736637df54b91d975
portellam <amportell.13@gmail.com>
2024-07-21 18:52:25 -0400

    Create __init__.py

---
+++ bin/powerstate-virtmanager.d/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python
+
+#
+# Filename:       __init__.py
+#
+
+from Bash.BashCommand   import BashCommand
+from Bash.BashDatatype  import BashDatatype
+from BashTests.BashCommandTests import BashCommandTests
+from Sudo.Sudo  import Sudo
