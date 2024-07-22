68c737da8a789053ceedfb760546c5d1fc0ef33c
portellam <amportell.13@gmail.com>
2024-07-22 10:52:58 -0400

    Added object and setter logic.

--- bin/powerstate-virtmanager.d/Bash/BashCommand.py
+++ bin/powerstate-virtmanager.d/Bash/BashCommand.py
@@ -12,22 +12,68 @@ import subprocess
 import sys
 
 class BashCommand:
+  code = 127
+  command = None
+  output = None
+
+  def __init__(self):
+    self.code = 1
+    self.command = None
+    self.output = None
+
+  def __init__(self, command):
+    self.code = 1
+    self.command = command
+    self.output = ""
+
+  # TODO: how to determine command and code from input ?
+  def SetInput():
+    try:
+      code = 127
+      output = sys.stdin.readline()
+      command = ""
+
+    except:
+      output = None
+      command = None
+
+  def SetOutput():
+    if command is None:
+      __init__()
+
+    try:
+      result = subprocess.run(
+        command,
+        capture_output = True,  # Python >= 3.7 only
+        text = True             # Python >= 3.7 only
+      )
+
+      code = result.return_code
+      output = result.stdout
+
+    except:
+      __init__()
+
   def GetInput():
     try:
-      result = sys.stdin.readline()
-      return result
+      return sys.stdin.readline()
 
     except Exception as contextManager:
-      print(exception)
-      sys.exit(1)
+      print(contextManager.exception)
+      return None
 
   def GetOutput(command):
     if command is None:
       return None
 
     try:
-      result = subprocess.run(command)
-      return result.output
+      result = subprocess.run(
+        command,
+        capture_output = True,  # Python >= 3.7 only
+        text = True             # Python >= 3.7 only
+      )
+
+      return result.stdout
 
     except:
       return ""
