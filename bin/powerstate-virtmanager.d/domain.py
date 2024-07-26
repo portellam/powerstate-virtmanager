fa35a568e5b058d64a4e792d7a365979882c4a36
portellam <amportell.13@gmail.com>
2024-07-25 23:18:35 -0400

    Delete domain.py

--- bin/powerstate-virtmanager.d/domain.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-#
-# Filename:       domain.py
-# Version:        1.0.0
-# Description:    Virtual Machine logic.
-# Author(s):      Alex Portell <github.com/portellam>
-# Maintainer(s):  Alex Portell <github.com/portellam>
-#
