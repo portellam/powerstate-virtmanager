a60326d91f61c58789ab6dfdaee18012f15e8ddb
portellam <amportell.13@gmail.com>
2024-07-25 23:18:31 -0400

    Moved sudo.

--- bin/powerstate_virtmanager_gui_d/sudo.py
+++ bin/powerstate_virtmanager_gui_d/sudo.py
@@ -1,19 +1,18 @@
 #!/usr/local/bin/python
 
 #
-# Filename:       Sudo.py
+# Filename:       sudo.py
 # Version:        1.0.0
 # Description:    Determine if the Python script was executed by a sudo user.
 # Author(s):      Alex Portell <github.com/portellam>
 # Maintainer(s):  Alex Portell <github.com/portellam>
 #
 
-from Bash.BashCommand import *
+from bash_command import BashCommand
 
 class Sudo:
   is_sudo = false
   command = "sudo"
 
   def __init__():
-    is_sudo = BashCommand.GetCode(command) == 0
-    self.is_sudo = is_sudo
+    self.is_sudo = BashCommand.GetCode(command) == 0
