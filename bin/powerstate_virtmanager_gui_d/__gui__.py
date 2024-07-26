791b56a04e3b159a5f852073d7c741f550f21190
portellam <amportell.13@gmail.com>
2024-07-25 23:19:07 -0400

    Renamed dir for gui source file.

---
+++ bin/powerstate_virtmanager_gui_d/__gui__.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python
+
+#
+# Filename:       __gui__.py
+# Description:    Sources for the Graphics User Interface.
+#
+
+from bash_command       import BashCommand
+from bash_datatype      import BashDatatype
+from domain             import Domain
+from sudo               import Sudo
