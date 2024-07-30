d8590d847e66db5f346824cfc1190cd78ca82cc2
portellam <amportell.13@gmail.com>
2024-07-30 17:06:59 -0400

    Added init py files for GUI and unit tests for GUI.

---
+++ bin/powerstate_virtmanager_gui_d/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#
+# Filename:       __init__.py
+# Description:    Sources for the Graphics User Interface.
+#
+
+from command      import Command
+from domain       import Domain
+from domain_list  import DomainList
+from sudo         import Sudo
---
+++ bin/powerstate_virtmanager_gui_d/__tests__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#
+# Filename:       __tests_init__.py
+# Description:    Unit test sources for the Graphics User Interface.
+#
+
+from command_tests      import CommandTests
+from domain_tests       import DomainTests
+from domain_list_tests  import DomainListTests
+from sudo_tests         import SudoTests
