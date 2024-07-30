6426423fb480d7b78b351ba530122739aecc27db
portellam <amportell.13@gmail.com>
2024-07-29 23:15:32 -0400

    Added logic to get list of domains, added constructor, and added properties.

---
+++ bin/powerstate_virtmanager_gui_d/domain_list.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+#
+# Filename:       domain.py
+# Version:        1.0.0
+# Description:    Virtual Machine logic.
+# Author(s):      Alex Portell <github.com/portellam>
+# Maintainer(s):  Alex Portell <github.com/portellam>
+#
+
+from command  import Command
+from domain   import Domain
+
+class DomainList:
+  list = []
+  selected_list = []
+  command = "virsh"
+  argument = "list"
+  command_suffix = "| head"
+
+  def __init__(self):
+    self.list = self.get_all()
+    self.selected_list = []
+
+  def get_command_with_option( \
+    self,
+    option
+  ):
+    return "{} {} {} {}".format( \
+      self.command,
+      self.argument,
+      option,
+      self.command_suffix
+    )
+
+  def get_all(self):
+    return Command.get_stdout_as_list( \
+      self.get_command_with_option("--all")
+    )
