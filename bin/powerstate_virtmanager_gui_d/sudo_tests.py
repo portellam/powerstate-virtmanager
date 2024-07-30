6fe206fd4987f52230b8eac61fb25daca02b221a
portellam <amportell.13@gmail.com>
2024-07-30 17:14:56 -0400

    Update sudo_tests.py

--- bin/powerstate_virtmanager_gui_d/sudo_tests.py
+++ bin/powerstate_virtmanager_gui_d/sudo_tests.py
@@ -19,5 +19,23 @@ from sudo     import Sudo
 class SudoTests(unittest.TestCase):
   placeholder = ""
 
+  @patch('os.system')
+  def test_is_root_command_executes_user_is_not_root_returns_false( \
+    self,
+    mock_os_system
+  ):
+    mock_os_system.return_value = 1
+    result = Sudo.is_root()
+    assert not result
+
+  @patch('os.system')
+  def test_is_root_command_executes_user_is_root_returns_true( \
+    self,
+    mock_os_system
+  ):
+    mock_os_system.return_value = 0
+    result = Sudo.is_root()
+    assert result
+
 if __name__ == '__main__':
   unittest.main()
