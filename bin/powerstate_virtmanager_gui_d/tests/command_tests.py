871ebe58740c4ba5d871c5bb57be21673386b47b
portellam <amportell.13@gmail.com>
2024-08-02 01:02:41 -0400

    Added working test.

--- bin/powerstate_virtmanager_gui_d/tests/command_tests.py
+++ bin/powerstate_virtmanager_gui_d/tests/command_tests.py
@@ -18,5 +18,21 @@ from ..sudo     import Sudo
 class CommandTests(unittest.TestCase):
   placeholder = ""
 
+  def test_get_code_throws_exception_return_one(self):
+    with patch.object(Command, 'set_completed_process') as \
+      mock_set_completed_process:
+
+      mock_set_completed_process.side_effect = Exception(Exception)
+
+    result1 = Command.get_code(self, None)
+    result2 = Command.get_code(self, "")
+    result3 = Command.get_code(self, "echo 419")
+
+    assert result1 == 1
+    assert result2 == 1
+    assert result3 == 1
+
+    mock_set_completed_process.assert_called_once
+
 if __name__ == '__main__':
   unittest.main()
