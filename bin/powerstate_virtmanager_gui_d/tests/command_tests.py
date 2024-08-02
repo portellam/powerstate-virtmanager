52c5c3b51e37b953692fe935334fa77e1316212b
portellam <amportell.13@gmail.com>
2024-08-02 01:29:12 -0400

    Added parameters to test.

--- bin/powerstate_virtmanager_gui_d/tests/command_tests.py
+++ bin/powerstate_virtmanager_gui_d/tests/command_tests.py
@@ -24,15 +24,38 @@ class CommandTests(unittest.TestCase):
 
       mock_set_completed_process.side_effect = Exception(Exception)
 
-    result1 = Command.get_code(self, None)
-    result2 = Command.get_code(self, "")
-    result3 = Command.get_code(self, "echo 419")
+      result1 = Command.get_code(self, None)
+      result2 = Command.get_code(self, "")
+      result3 = Command.get_code(self, "echo 419")
 
-    assert result1 == 1
-    assert result2 == 1
-    assert result3 == 1
+      assert result1 == 1
+      assert result2 == 1
+      assert result3 == 1
+      mock_set_completed_process.assert_called_once
 
-    mock_set_completed_process.assert_called_once
+  @pytest.mark.parametrize(
+    "code",
+    [
+      0,
+      1,
+      127
+    ],
+  )
+  def test_get_code_succeeds_return_expected_code(code):
+    with patch.object(Command, 'set_completed_process') as mock_set_completed_process:
+
+      mock_set_completed_process.side_effect = None
+      command = Command()
+      command.code = code
+
+      result1 = command.get_code(None)
+      result2 = command.get_code("False")
+      result3 = command.get_code("True")
+
+      assert result1 == code
+      assert result2 == code
+      assert result3 == code
+      mock_set_completed_process.assert_called_once
 
 if __name__ == '__main__':
   unittest.main()
