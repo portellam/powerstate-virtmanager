7b72c95349ceea60a9035e1f17d70f99b65a549c
portellam <amportell.13@gmail.com>
2024-08-08 17:36:59 -0400

    Added logic to check for if a dependency does not exist. Removed unused imports.

--- bin/powerstate_virtmanager_gui_d/tests/domain_tests.py
+++ bin/powerstate_virtmanager_gui_d/tests/domain_tests.py
@@ -8,15 +8,36 @@
 # Maintainer(s):  Alex Portell <github.com/portellam>
 #
 
-import pytest
+# TODO: add enum for multiple dependencies.
+
 import unittest
-from unittest.mock  import Mock, patch
+from unittest.mock  import patch
 
 from ..command  import Command
 from ..domain   import Domain
 
 class DomainTests(unittest.TestCase):
   placeholder = ""
+
+  def test__does_dependency_exist(self):
+    dependency = "virsh"
+    check_dependency = "command -v {}".format(dependency)
+    command = Command(check_dependency)
+
+    try:
+      command.run()
+      is_available = command.code == 0
+
+    except:
+      print("Could not determine if dependency '{}' exists.".format(dependency))
+      assert False
+
+    if not is_available:
+      print("Dependency '{}' is not available.".format(dependency))
+      assert False
+
+    print("Dependency '{}' is available.".format(dependency))
+    assert True
 
 if __name__ == '__main__':
   unittest.main()
